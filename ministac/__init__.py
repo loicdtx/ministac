a3764872e9bec18aca635aebaa02031d480c7d22
Loïc Dutrieux <loic.dutrieux@gmail.com>
2018-12-10 23:27:42 +0100

    Implemented filter, indices, datetime field. Untested

--- ministac/__init__.py
+++ ministac/__init__.py
@@ -1,4 +1,5 @@
 from jsonschema import validate
+from sqlalchemy.types import Numeric
 
 from ministac.db import session_scope
 from ministac.models import Item, Collection
@@ -35,24 +36,32 @@ def add_collection(collection):
         session.add(collection_obj)
 
 
-def search(collection, geom, begin, end, max_cloud_cover=100):
+def search(collection, geom=None, startDate=None, endDate=None,
+           maxCloudCover=None):
     """Query the database for matching items
 
     Args:
         collection (str): The name of the collection to query
         geom (dict): A geojson like geometry
-        begin (datetime.datetime): Begin date
-        end (datetime.datetime): End date
-        max_cloud_cover (float): Maximum cloud cover allowed in percent (value
+        startDate (datetime.datetime): Begin date
+        endDate (datetime.datetime): End date
+        maxCloudCover (float): Maximum cloud cover allowed in percent (value
             between 0 and 100)
     """
-    geom_wkt = shape(geom).wkt
     with session_scope() as session:
         objects = session.query(Item)\
                 .join(Item.collection)\
-                .filter(Collection.name==collection)\
-                .filter(Item.geom.ST_Intersects(geom_wkt))
-        # TODO: Missing date filter and cc filter
+                .filter(Collection.name==collection)
+        if geom is not None:
+            geom_wkt = shape(geom).wkt
+            objects = objects.filter(Item.geom.ST_Intersects(geom_wkt))
+        if startDate is not None:
+            objects = objects.filter(Item.time >= startDate)
+        if endDate is not None:
+            objects = objects.filter(Item.time >= endDate)
+        if maxCloudCover is not None:
+            objects = objects.filter(Item.meta['properties']['eo:cloud_cover']\
+                                     .astext.cast(Numeric) <= maxCloudCover)
         return [x.geojson for x in objects]
 
 
--- ministac/models.py
+++ ministac/models.py
@@ -1,12 +1,14 @@
+import datetime as dt
+
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy import DateTime, UniqueConstraint
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from geoalchemy2 import Geometry
+from geoalchemy2.shape import from_shape, to_shape
 
 from shapely.geometry import shape, mapping
-from geoalchemy2.shape import from_shape, to_shape
 from jsonschema import validate
 
 from ministac.db import Base
@@ -26,15 +28,26 @@ class Item(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     geom = Column(Geometry())
+    time = Column(DateTime, index=True)
     meta = Column(JSONB)
     time_created = Column(DateTime(timezone=True), server_default=func.now())
-    collection_id = Column(Integer, ForeignKey('collection.id'))
+    collection_id = Column(Integer, ForeignKey('collection.id'), index=True)
     UniqueConstraint(collection_id, name)
 
     collection = relationship("Collection", back_populates="items")
 
     @classmethod
     def from_geojson(cls, feature, collection, session=None):
+        """Class method to instantiate an Item object from a metadata description
+
+        Args:
+            feature (dict): A geojson feature that complies with the item json
+                schema
+            collection (str or ministac.models.Collection): Collection to which
+                the item belong
+            session (Session): Optional database session. Only required when collection is
+                a string.
+        """
         # Load collection object
         if isinstance(collection, str):
             collection = session.query(Collection).filter_by(name=collection).first()
@@ -42,9 +55,13 @@ class Item(Base):
         jsonschema.validate(feature, ITEM_SCHEMA)
         # Build geom
         geom = from_shape(shape(feature['geometry']), 4326)
+        # Parse datetime
+        t = dt.datetime.strptime(feature['properties']['datetime'],
+                                 '%Y-%m-%dT%H:%M:%SZ')
         # Build object
         return cls(name=feature['id'],
                    geom=geom,
+                   time=t,
                    meta=feature,
                    collection=collection)
 
